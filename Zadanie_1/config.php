33577e0eb2862ac4fb23b65bd03acaf4c385ac82
Bartek <baqbaek@gmail.com>
2024-03-23 13:48:43 +0100

    folder rename

--- Zadanie_1/config.php
+++ Zadanie_1/config.php
@@ -1,7 +1,7 @@
 <?php
 define('_SERVER_NAME', 'localhost:80');
 define('_SERVER_URL', 'http://'._SERVER_NAME);
-define('_APP_ROOT', '/projects/PSS_PHP/widok_kontroler');
+define('_APP_ROOT', '/projects/PSS_PHP/0_1widok_kontroler');
 define('_APP_URL', _SERVER_URL._APP_ROOT);
 define("_ROOT_PATH", dirname(__FILE__));
 ?>
